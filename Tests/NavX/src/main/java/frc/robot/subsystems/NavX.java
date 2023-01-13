858d6d753a859f103b7c24aaa856d3e4c06f5031
4917 Glados <software@4917.ca>
2023-01-13 16:27:08 -0500

    NavX info and examples Xavier, Jonah, Luke

---
+++ Tests/NavX/src/main/java/frc/robot/subsystems/NavX.java
@@ -0,0 +1,20 @@
+//NavX (aka ahrs) Communication with robot MXP SPI
+
+//C:\Users\4917\navx-mxp\java\examples\AutoBalance\src\org\usfirst\frc\team2465\robot
+
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NavX extends SubsystemBase {
+  /** Creates a new NavX. */
+  public NavX() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
