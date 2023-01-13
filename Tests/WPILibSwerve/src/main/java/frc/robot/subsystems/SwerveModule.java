c8e5cab3c0dc411060252e08d62a2db9f2444d69
Eric Gaudet <ericjpgaudet@yahoo.ca>
2023-01-13 13:43:26 -0500

    SwerveModule ported to our hardware but not tested

--- Tests/WPILibSwerve/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ Tests/WPILibSwerve/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,36 +1,114 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.SensorTimeBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 public class SwerveModule {
+  /**
+   * Constants for this module
+   */
+  public static final class ModuleConstants {
+    public static final double kMaxModuleAngularSpeedRadiansPerSecond = 2 * Math.PI;
+    public static final double kMaxModuleAngularAccelerationRadiansPerSecondSquared = 2 * Math.PI;
+
+    public static final int kDriveEncoderCPR = 2048; // For CTRE TalonFX built-in encoder
+    public static final int kTurningEncoderCPR = 4096; // For CTRE CANCoder magnetic encoder
+    public static final double kWheelDiameterMeters = 0.15;
+    public static final double kDriveEncoderDistancePerPulse =
+        // Assumes the encoders are directly mounted on the wheel shafts
+        (kWheelDiameterMeters * Math.PI) / (double) kDriveEncoderCPR;
+
+    public static final double kTurningEncoderDistancePerPulse =
+        // Assumes the encoders are on a 1:1 reduction with the module shaft.
+        (2 * Math.PI) / (double) kTurningEncoderCPR;
+
+    public static final double kPModuleTurningController = 1;
+
+    public static final double kPModuleDriveController = 1;
+  }
+
+  // Member variables
   private final TalonFX m_driveMotor;
   private final CANSparkMax m_turningMotor;
   private final CANCoder m_turningEncoder;
 
-  public SwerveModule(int driveMotorId, int turningMotorId, int turningEncoderId) {
+  private final PIDController m_drivePIDController =
+      new PIDController(1.0, 0, 0);
+  private final ProfiledPIDController m_turningPIDController =
+      new ProfiledPIDController(
+          ModuleConstants.kPModuleTurningController,
+          0,
+          0,
+          new TrapezoidProfile.Constraints(
+              ModuleConstants.kMaxModuleAngularSpeedRadiansPerSecond,
+              ModuleConstants.kMaxModuleAngularAccelerationRadiansPerSecondSquared));
+
+  /**
+   * Construct the SwerveModule
+   * 
+   * @param driveMotorId CAN ID for the drive motor
+   * @param turningMotorId CAN ID for the turning motor
+   * @param turningEncoderId CAN ID for the turning encoder
+   * @param bReverseTurningEncoderDirection Set to true to reverse the default sensor direction
+   */
+  public SwerveModule(int driveMotorId, int turningMotorId, int turningEncoderId, boolean bReverseTurningEncoderDirection) {
+    // Create all the hardware objects
     m_driveMotor = new TalonFX(driveMotorId);
     m_turningMotor = new CANSparkMax(turningMotorId, CANSparkMaxLowLevel.MotorType.kBrushed);
     m_turningEncoder = new CANCoder(turningEncoderId);
 
-    //...
+    // Configure all the hardware objects as needed (including drive motor encoder)
+    m_driveMotor.configSelectedFeedbackCoefficient(ModuleConstants.kDriveEncoderDistancePerPulse);
+
+    m_turningEncoder.configSensorDirection(bReverseTurningEncoderDirection);  // False means positive rotation occurs when magnet is spun counter-clockwise when observer is facing the LED side of CANCoder.
+    m_turningEncoder.configFeedbackCoefficient(ModuleConstants.kTurningEncoderDistancePerPulse, "radians", SensorTimeBase.PerSecond);
+
+    // Configure the PID controllers
+    // Limit the PID Controller's input range between -pi and pi and set the input to be continuous.
+    m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+  }
+
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(
+        m_driveMotor.getSelectedSensorVelocity(), new Rotation2d(m_turningEncoder.getPosition()));
   }
 
   public SwerveModulePosition getPosition() {
-    return new SwerveModulePosition();
+    return new SwerveModulePosition(
+        m_driveMotor.getSelectedSensorPosition(), new Rotation2d(m_turningEncoder.getPosition()));
   }
 
   public void setDesiredState(SwerveModuleState desiredState) {
-    //m_driveMotor.set(, 0.0);
-    m_turningMotor.set(0.0);
+    // Optimize the reference state to avoid spinning further than 90 degrees
+    SwerveModuleState state =
+      SwerveModuleState.optimize(desiredState, new Rotation2d(m_turningEncoder.getPosition()));
+
+    // Calculate the drive output from the drive PID controller.
+    final double driveOutput =
+      m_drivePIDController.calculate(m_driveMotor.getSelectedSensorVelocity(), state.speedMetersPerSecond);
+
+    // Calculate the turning motor output from the turning PID controller.
+    final double turnOutput =
+      m_turningPIDController.calculate(m_turningEncoder.getPosition(), state.angle.getRadians());
+
+    // Calculate the turning motor output from the turning PID controller.
+    m_driveMotor.set(ControlMode.PercentOutput, driveOutput);
+    m_turningMotor.set(turnOutput);
   }
 
   public void resetEncoders() {
+    m_driveMotor.setSelectedSensorPosition(0.0);
     m_turningEncoder.setPosition(0.0);
   }
 }
