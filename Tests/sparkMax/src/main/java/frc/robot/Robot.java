3039e4af4dabc2dd4a2da0f03eabc8106638d265
4917 Roomba <software@4917.ca>
2023-01-14 09:27:18 -0500

    Spark Max example

--- Tests/sparkMax/src/main/java/frc/robot/Robot.java
+++ Tests/sparkMax/src/main/java/frc/robot/Robot.java
@@ -1,103 +1,167 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
+
+// package frc.robot;
+
+// import edu.wpi.first.wpilibj.TimedRobot;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+// /**
+//  * The VM is configured to automatically run this class, and to call the functions corresponding to
+//  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+//  * the package after creating this project, you must also update the build.gradle file in the
+//  * project.
+//  */
+// public class Robot extends TimedRobot {
+//   private Command m_autonomousCommand;
+
+//   private RobotContainer m_robotContainer;
+
+//   /**
+//    * This function is run when the robot is first started up and should be used for any
+//    * initialization code.
+//    */
+//   @Override
+//   public void robotInit() {
+//     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+//     // autonomous chooser on the dashboard.
+//     m_robotContainer = new RobotContainer();
+//   }
+
+//   /**
+//    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+//    * that you want ran during disabled, autonomous, teleoperated and test.
+//    *
+//    * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+//    * SmartDashboard integrated updating.
+//    */
+//   @Override
+//   public void robotPeriodic() {
+//     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+//     // commands, running already-scheduled commands, removing finished or interrupted commands,
+//     // and running subsystem periodic() methods.  This must be called from the robot's periodic
+//     // block in order for anything in the Command-based framework to work.
+//     CommandScheduler.getInstance().run();
+//   }
+
+//   /** This function is called once each time the robot enters Disabled mode. */
+//   @Override
+//   public void disabledInit() {}
+
+//   @Override
+//   public void disabledPeriodic() {}
+
+//   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+//   @Override
+//   public void autonomousInit() {
+//     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+//     // schedule the autonomous command (example)
+//     if (m_autonomousCommand != null) {
+//       m_autonomousCommand.schedule();
+//     }
+//   }
+
+//   /** This function is called periodically during autonomous. */
+//   @Override
+//   public void autonomousPeriodic() {}
+
+//   @Override
+//   public void teleopInit() {
+//     // This makes sure that the autonomous stops running when
+//     // teleop starts running. If you want the autonomous to
+//     // continue until interrupted by another command, remove
+//     // this line or comment it out.
+//     if (m_autonomousCommand != null) {
+//       m_autonomousCommand.cancel();
+//     }
+//   }
+
+//   /** This function is called periodically during operator control. */
+//   @Override
+//   public void teleopPeriodic() {}
+
+//   @Override
+//   public void testInit() {
+//     // Cancels all running commands at the start of test mode.
+//     CommandScheduler.getInstance().cancelAll();
+//   }
+
+//   /** This function is called periodically during test mode. */
+//   @Override
+//   public void testPeriodic() {}
+
+//   /** This function is called once when the robot is first started up. */
+//   @Override
+//   public void simulationInit() {}
+
+//   /** This function is called periodically whilst in simulation. */
+//   @Override
+//   public void simulationPeriodic() {}
+// }
+
+
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  private DifferentialDrive m_myRobot;
+  private Joystick m_leftStick;
+  private Joystick m_rightStick;
+  private static final int leftDeviceID = 1; 
+  private static final int rightDeviceID = 2;
+  private CANSparkMax m_leftMotor;
+  private CANSparkMax m_rightMotor;
 
-  private RobotContainer m_robotContainer;
-
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
-  }
-
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
+   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+   * 
+   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
+   * first parameter
+   * 
+   * The motor type is passed as the second parameter. Motor type can either be:
+   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
+   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
+   * 
+   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
+   * these parameters to match your setup
    */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+    m_leftMotor = new CANSparkMax(leftDeviceID, MotorType.kBrushless);
+    m_rightMotor = new CANSparkMax(rightDeviceID, MotorType.kBrushless);
+
+    /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+    m_leftMotor.restoreFactoryDefaults();
+    m_rightMotor.restoreFactoryDefaults();
+
+    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+    m_leftStick = new Joystick(0);
+    m_rightStick = new Joystick(1);
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+  public void teleopPeriodic() {
+    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
   }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
 }
--- Tests/sparkMax/src/main/java/frc/robot/subsystems/sparkMaxExamples.java
+++ Tests/sparkMax/src/main/java/frc/robot/subsystems/sparkMaxExamples.java
@@ -1,17 +1,83 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+/*
+ * 
+ * 
+ * REV Hardware client overveiw: https://docs.revrobotics.com/rev-hardware-client/
+ * REV Spark Max User Manual: https://docs.revrobotics.com/sparkmax/ 
+ * 
+ * 
+ */
 
-package frc.robot.subsystems;
+//  package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class sparkMaxExamples extends SubsystemBase {
-  /** Creates a new sparkMaxExamples. */
-  public sparkMaxExamples() {}
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-}
+//  import edu.wpi.first.wpilibj.Joystick;
+//  import edu.wpi.first.wpilibj.TimedRobot;
+ 
+//  import com.revrobotics.CANSparkMax;
+//  import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+ 
+//  /**
+//   * This sample program shows how to control a motor using a joystick. In the
+//   * operator control part of the program, the joystick is read and the value is
+//   * written to the motor.
+//   *
+//   * <p>Joystick analog values range from -1 to 1 and speed controller inputs also
+//   * range from -1 to 1 making it easy to work together.
+//   */
+//  public class Robot extends TimedRobot {
+//    private static final int leadDeviceID = 1;
+//    private static final int followDeviceID = 2;
+//    private static final int kJoystickPort = 0;
+ 
+//    private CANSparkMax m_leadMotor;
+//    private CANSparkMax m_followMotor;
+//    private Joystick m_joystick;
+ 
+//    @Override
+//    public void robotInit() {
+//      /**
+//       * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+//       * 
+//       * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
+//       * first parameter
+//       * 
+//       * The motor type is passed as the second parameter. Motor type can either be:
+//       *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
+//       *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
+//       * 
+//       * The example below initializes two brushless motors with CAN IDs 1 and 2. Change
+//       * these parameters to match your setup
+//       */
+//      m_leadMotor = new CANSparkMax(leadDeviceID, MotorType.kBrushless);
+//      m_followMotor = new CANSparkMax(followDeviceID, MotorType.kBrushless);
+ 
+//      /**
+//       * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+//       * in the SPARK MAX to their factory default state. If no argument is passed, these
+//       * parameters will not persist between power cycles
+//       */
+//      m_leadMotor.restoreFactoryDefaults();
+//      m_followMotor.restoreFactoryDefaults();
+ 
+//      /**
+//       * In CAN mode, one SPARK MAX can be configured to follow another. This is done by calling
+//       * the follow() method on the SPARK MAX you want to configure as a follower, and by passing
+//       * as a parameter the SPARK MAX you want to configure as a leader.
+//       */
+//      m_followMotor.follow(m_leadMotor);
+ 
+//      m_joystick = new Joystick(kJoystickPort);
+//    }
+ 
+//    @Override
+//    public void teleopPeriodic() {
+//      /**
+//       * m_followMotor will automatically follow whatever the applied output is on m_leadMotor.
+//       * 
+//       * Thus, set only needs to be called on m_leadMotor to control both of them
+//       */
+//      m_leadMotor.set(m_joystick.getY());
+//    }
+//  }
