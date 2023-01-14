2c41a4087d23455341430b9e6d07e2d36416f829
4917 Sonny <software@4917.ca>
2023-01-14 15:34:14 -0500

    Working TalonFX Example
    - completed& tested example code
    - added manual link under TalonFXSub
    -added api

---
+++ Tests/TalonFX/src/main/java/frc/robot/commands/RunMotorCmd.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.TalonFXSub;
+
+public class RunMotorCmd extends CommandBase {
+  TalonFXSub m_talonFXSub;
+
+  /** Creates a new RunMotorCmd. */
+  public RunMotorCmd(TalonFXSub talonFXSub) {
+    m_talonFXSub = talonFXSub;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_talonFXSub.setMotorPower(0.2);
+  }
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_talonFXSub.setMotorPower(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
