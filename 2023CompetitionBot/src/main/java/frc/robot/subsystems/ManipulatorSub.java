c150e518efc9866ca53f2f9a86627addaa4114a7
4917 Claptrap <software@4917.ca>
2023-01-20 16:29:36 -0500

    RotateArm

--- 2023CompetitionBot/src/main/java/frc/robot/subsystems/ManipulatorSub.java
+++ 2023CompetitionBot/src/main/java/frc/robot/subsystems/ManipulatorSub.java
@@ -4,14 +4,25 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 
 public class ManipulatorSub extends SubsystemBase {
+  
+  private final CANSparkMax m_armMotor = 
+      new CANSparkMax(Constants.CanIds.kRightDriveMotor, CANSparkMaxLowLevel.MotorType.kBrushless);
+
   /** Creates a new TemplateSub. */
   public ManipulatorSub() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  public void rotateArm(double armPower) {
+    m_armMotor.set(armPower);
   }
 }
