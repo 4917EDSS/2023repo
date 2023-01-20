c902876c38f4fb6f6d5a6132214d4117d86812e9
4917 Ultron <software@4917.ca>
2023-01-20 16:27:54 -0500

    ControllerButtonTest Created

--- Tests/ControllerTest/ControllerTest/src/main/java/frc/robot/RobotContainer.java
+++ Tests/ControllerTest/ControllerTest/src/main/java/frc/robot/RobotContainer.java
@@ -5,13 +5,9 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.DriveForwardCmd;
-import frc.robot.commands.DriveWithJoystickCmd;
-import frc.robot.subsystems.DrivetrainSub;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
-
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,19 +17,15 @@ import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final DrivetrainSub m_drivetrainSub = new DrivetrainSub();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandPS4Controller m_driverController =
-      new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
+  private final CommandPS4Controller m_driverController = 
+    new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
-
-    // set default command for subsystems.
-    m_drivetrainSub.setDefaultCommand(new DriveWithJoystickCmd(m_driverController, m_drivetrainSub));
   }
 
   /**
@@ -46,9 +38,30 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+
+    m_driverController.square().onTrue(new PrintCommand("Square Pressed"));
+    m_driverController.circle().onTrue(new PrintCommand("Circle Pressed"));
+    m_driverController.triangle().onTrue(new PrintCommand("Triangle Pressed"));
     m_driverController.cross().onTrue(new PrintCommand("Cross Pressed"));
-    
-    m_driverController.square().whileTrue(new DriveForwardCmd(m_drivetrainSub));
+
+    m_driverController.povUp().onTrue(new PrintCommand("POV Up Pressed"));
+    m_driverController.povRight().onTrue(new PrintCommand("POV Right Pressed"));
+    m_driverController.povDown().onTrue(new PrintCommand("POV Down Pressed"));
+    m_driverController.povLeft().onTrue(new PrintCommand("POV Left Pressed"));
+    m_driverController.povUpLeft().onTrue(new PrintCommand("POV Top Left Pressed"));
+    m_driverController.povUpRight().onTrue(new PrintCommand("POV Top Right Pressed"));
+    m_driverController.povDownRight().onTrue(new PrintCommand("POV Bottom Right Pressed"));
+    m_driverController.povDownLeft().onTrue(new PrintCommand("POV Bottom Left Pressed"));
+
+    m_driverController.share().onTrue(new PrintCommand("Share Pressed"));
+    m_driverController.options().onTrue(new PrintCommand("Options Pressed"));
+    m_driverController.touchpad().onTrue(new PrintCommand("Touchpad Pressed"));
+
+    m_driverController.L1().onTrue(new PrintCommand("Left Bumper Pressed"));
+    m_driverController.L2().onTrue(new PrintCommand("Left Trigger Pressed"));
+    m_driverController.R1().onTrue(new PrintCommand("Right Bumper Pressed"));
+    m_driverController.R2().onTrue(new PrintCommand("Right Trigger Pressed"));
+
   }
 
   /**
@@ -58,6 +71,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return new PrintCommand("This code has no auto");
+    return new PrintCommand("No auto coded yet");
   }
 }
