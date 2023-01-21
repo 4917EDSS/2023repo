cb3ebe5d8a959dca358583a067b60b208d9e5b22
4917 Roomba <software@4917.ca>
2023-01-21 14:37:12 -0500

    Riley's Double Solenoids example code
    Description

---
+++ Tests/DoubleSolenoids/src/main/java/frc/robot/commands/SetValveCommand.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.DoubleSolenoidsSub;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SetValveCommand extends CommandBase {
+  /** Creates a new SetValveCommand. */
+  private final DoubleSolenoidsSub m_subsystem;
+  public SetValveCommand(DoubleSolenoidsSub subsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_subsystem = subsystem;
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+    m_subsystem.setValve(Value.kForward);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
