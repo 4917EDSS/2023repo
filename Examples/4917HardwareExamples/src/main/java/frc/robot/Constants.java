3765f13cd5c84cdb7eb4e21bddfda21823e548ef
4917 Glados <software@4917.ca>
2023-01-21 14:16:42 -0500

    Encoder position

--- Examples/4917HardwareExamples/src/main/java/frc/robot/Constants.java
+++ Examples/4917HardwareExamples/src/main/java/frc/robot/Constants.java
@@ -15,7 +15,7 @@ package frc.robot;
 public final class Constants {
   public static class CanIds {
     public static final int talonFxId = 1;
-    public static final int spartMaxId = 2;
+    public static final int sparkMaxId = 2;
   }
 
   public static class DioIds {
