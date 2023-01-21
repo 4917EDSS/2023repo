ebc10f963d6398c349b9bd8573af97b0046bf419
4917 Roomba <software@4917.ca>
2023-01-21 16:16:20 -0500

    Riley's *FIXED* Solenoid code
    Description

--- Tests/Solenoids/Solenoids/src/main/java/frc/robot/RobotContainer.java
+++ Tests/Solenoids/Solenoids/src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import frc.robot.commands.SetValveCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Solenoids;
 
+
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
