d0d2419e6138e3571b79f944a8b9b4431cae66e8
4917-Victus <software@4917.ca>
2023-01-23 17:23:59 -0500

    Operator and Driver were set to the same port for some reason

--- 2023CompetitionBot/src/main/java/frc/robot/RobotContainer.java
+++ 2023CompetitionBot/src/main/java/frc/robot/RobotContainer.java
@@ -41,7 +41,7 @@ public class RobotContainer {
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandPS4Controller m_driverController = new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
-  private final CommandPS4Controller m_operatorController = new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
+  private final CommandPS4Controller m_operatorController = new CommandPS4Controller(OperatorConstants.kOperatorControllerPort);
   
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
