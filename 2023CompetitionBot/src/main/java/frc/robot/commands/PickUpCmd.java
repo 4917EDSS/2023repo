156ccce5bec479019811d747bc4447d7404959de
4917 CAD <software@4917.ca>
2023-02-10 16:18:08 -0500

    changed armAngle to armPosition and moved setPosition method to initialize

--- 2023CompetitionBot/src/main/java/frc/robot/commands/PickUpCmd.java
+++ 2023CompetitionBot/src/main/java/frc/robot/commands/PickUpCmd.java
@@ -10,15 +10,15 @@ import frc.robot.subsystems.MastSub;
 import frc.robot.subsystems.SubControl.Mode;
 
 public class PickUpCmd extends CommandBase {
-  private final static double kStationArmAngle = -45;
+  private final static double kStationArmPosition = -45;
   private final static double kStationMastPosition = 13;
-  private final static double kGroundArmAngle = -30;
+  private final static double kGroundArmPosition = -30;
   private final static double kGroundMastPosition = 30;
 
   public final static int kSquareButton = 0;
   public final static int kOptionsButton = 1;
 
-  private double m_armAngle = 0;
+  private double m_armPosition = 0;
   private double m_mastPosition = 0;
   private final static double kMaxArmPower = 0.5; // <----- Tune this value 
   private final static double kMaxMastPower = 0.5; // <----- Tune this value 
@@ -34,10 +34,10 @@ public class PickUpCmd extends CommandBase {
     addRequirements(armSub,mastSub);
 
     if(button == kSquareButton) {
-      m_armAngle = kStationArmAngle;
+      m_armPosition = kStationArmPosition;
       m_mastPosition = kStationMastPosition;
     } else if(button == kOptionsButton) {
-      m_armAngle = kGroundArmAngle;
+      m_armPosition = kGroundArmPosition;
       m_mastPosition = kGroundMastPosition;
     }
     // Use addRequirements() here to declare subsystem dependencies.
@@ -45,13 +45,14 @@ public class PickUpCmd extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_armSub.setPosition(Mode.MANUAL,kMaxArmPower, m_armPosition); 
+    m_mastSub.setPosition(Mode.MANUAL, kMaxMastPower, m_mastPosition);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_armSub.setPosition(Mode.MANUAL,kMaxArmPower, m_armAngle); 
-    m_mastSub.setPosition(Mode.MANUAL, kMaxMastPower, m_mastPosition);
     //m_manipulatorSub.autoSetMastPosition(m_mastPosition);
   }
 
@@ -65,17 +66,17 @@ public class PickUpCmd extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    boolean isArmAngleInPosition = false;
+    boolean isArmInPosition = false;
     boolean isMastInPosition = false;
 
-    if((m_armSub.getPosition() >= (m_armAngle - 5)) && (m_armSub.getPosition() <= (m_armAngle + 5))) {
-      isArmAngleInPosition = true;
+    if((m_armSub.getPosition() >= (m_armPosition - 5)) && (m_armSub.getPosition() <= (m_armPosition + 5))) {
+      isArmInPosition = true;
     }
     if((m_mastSub.getPosition() >= (m_mastPosition - 5))
         && (m_mastSub.getPosition() <= (m_mastPosition + 5))) {
       isMastInPosition = true;
     }
 
-    return (isArmAngleInPosition && isMastInPosition);
+    return (isArmInPosition && isMastInPosition);
   }
 }
