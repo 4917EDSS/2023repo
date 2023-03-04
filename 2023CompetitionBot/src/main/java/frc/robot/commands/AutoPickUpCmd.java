74b979bf6e8d4616774fdb3c0b4b7ec2a79ee166
4917 Roomba <software@4917.ca>
2023-03-04 16:29:06 -0500

    Update AutoPickUpCmd.java
    whoops

--- 2023CompetitionBot/src/main/java/frc/robot/commands/AutoPickUpCmd.java
+++ 2023CompetitionBot/src/main/java/frc/robot/commands/AutoPickUpCmd.java
@@ -1,19 +1,30 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// Copyright © 2023 Riley Fisher. No rights reserved.
 
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.ArmSub;
+import frc.robot.subsystems.DrivetrainSub;
+import frc.robot.subsystems.IntakeSub;
+import frc.robot.subsystems.MastSub;
+import frc.robot.subsystems.ManipulatorsPositions;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class AutoPickUpCmd extends SequentialCommandGroup {
   /** Creates a new AutoPickUpCmd. */
-  public AutoPickUpCmd() {
+  public AutoPickUpCmd(ManipulatorsPositions positions, ArmSub armSub, MastSub mastSub, IntakeSub intakeSub, DrivetrainSub drivetrainSub, double targetDriveDistance, double power) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    
+    /*\ Sets the intake position, drives in, then grabs \*/
+    addCommands(
+    new IntakeSetPositionCmd(positions, armSub, mastSub, intakeSub), 
+    new DriveStraightCmd(drivetrainSub, targetDriveDistance), 
+    new IntakeGamePieceCmd(power, intakeSub));
   }
 }
