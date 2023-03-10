b7a1ce56625b9da1b206b798feb0550d91cbe08f
4917-Baymax <software@4917.ca>
2023-03-10 16:06:02 -0500

    changed calcHoldPower to calcMovePower
    Description

--- 2023CompetitionBot/src/main/java/frc/robot/subsystems/MastSub.java
+++ 2023CompetitionBot/src/main/java/frc/robot/subsystems/MastSub.java
@@ -247,7 +247,7 @@ public class MastSub extends SubsystemBase {
       case HOLDING:
         // If the mechanism is at it's target location, apply power to hold it there if necessary
         // TODO: Check if we can use the calcMovePower function since the PID could take care of both cases
-        newPower = calcHoldPower(currentPosition, m_currentControl.targetPosition);
+        newPower = calcMovePower(currentPosition, m_currentControl.targetPosition, m_currentControl.targetPower);
         break;
 
       case INTERRUPTED:
@@ -256,7 +256,7 @@ public class MastSub extends SubsystemBase {
           m_currentControl.state = State.MOVING;
           // Otherwise, hold this position
         } else {
-          newPower = calcHoldPower(currentPosition, m_blockedPosition);
+          newPower = calcMovePower(currentPosition, m_currentControl.targetPosition, m_currentControl.targetPower);
         }
         break;
 
@@ -279,7 +279,9 @@ public class MastSub extends SubsystemBase {
 
 
   private double calcHoldPower(double currentPosition, double targetPosition) {
-    return 0;
+    double holdPower = currentPosition - targetPosition;
+
+    return holdPower;
   }
 
   /** Display/get subsystem information to/from the Smart Dashboard */
