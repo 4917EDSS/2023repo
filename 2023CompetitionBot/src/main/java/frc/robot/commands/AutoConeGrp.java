4513180bef1df7a51ffa7e57574f82b0bf98159f
Sam Maier <sam@sammaier.ca>
2023-03-16 09:51:22 -0400

    We got our extra clearance on the wrong drive
    We should drive extra far when backing off - not when coming in.

--- 2023CompetitionBot/src/main/java/frc/robot/commands/AutoConeGrp.java
+++ 2023CompetitionBot/src/main/java/frc/robot/commands/AutoConeGrp.java
@@ -40,9 +40,9 @@ public class AutoConeGrp extends SequentialCommandGroup {
         new SetLimitSwitchesCmd(mastSub, armSub, intakeSub),
         new IntakeSetPositionCmd(ManipulatorsPositions.HIGH_CONE, m_armSub, m_mastSub, m_intakeSub),
         new WaitCommand(0.25),
-        new DriveStraightCmd(drivetrainSub, (-(clearanceDistance + 0.1))),
+        new DriveStraightCmd(drivetrainSub, (-clearanceDistance)),
         new ExpelGamePieceCmd(0.5, m_intakeSub),
-        new DriveStraightCmd(drivetrainSub, (clearanceDistance)),
+        new DriveStraightCmd(drivetrainSub, (clearanceDistance + 0.1)),
         new IntakeSetPositionCmd(ManipulatorsPositions.HOME, m_armSub, m_mastSub, m_intakeSub));
   }
 }
